8a2f9372b051e60bd4d5b96b33a06783c60da2c8
Andrey <andrey.makarich@gmail.com>
2018-07-22 17:07:06 +0300

    test project + test scene + get project call

---
+++ Assets/IceHammer/DTO/Project.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace IceHammer {
+
+    [Serializable]
+    public class Project {
+        public string id;
+        public string name;
+        public string[] sheets;
+    }
+
+}
+
---
+++ Assets/IceHammer/IceHammerTestComponent.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+public class IceHammerTestComponent : MonoBehaviour {
+
+	public static readonly string projectID = "kCeAKCduEOmlBgQgis23otP6VsT3Tvq9";
+
+
+	void Start() {
+        StartCoroutine(GetSheets());
+    }
+ 
+    IEnumerator GetSheets() {
+        UnityWebRequest www = UnityWebRequest.Get("https://app.icehammer.org/api/project/" + projectID);
+        yield return www.SendWebRequest();
+
+        if(www.isNetworkError || www.isHttpError) {
+            Debug.Log(www.error);
+        }
+        else {
+        	IceHammer.Project project = JsonUtility.FromJson<IceHammer.Project>(www.downloadHandler.text);
+            Debug.Log(project.id);
+            Debug.Log(project.name);
+            Debug.Log(project.sheets);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
